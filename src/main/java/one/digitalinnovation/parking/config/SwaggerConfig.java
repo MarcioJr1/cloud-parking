42a89143f6ef0e424492614efea6f36aaabe419f
Marcio Jr <marcio_jr2007@hotmail.com>
2022-08-20 23:47:17 -0300

    Final

--- src/main/java/one/digitalinnovation/parking/config/SwaggerConfig.java
+++ src/main/java/one/digitalinnovation/parking/config/SwaggerConfig.java
@@ -4,10 +4,15 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.*;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 @Component
 @EnableSwagger2
@@ -19,7 +24,27 @@ public class SwaggerConfig {
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("one.digitalinnovation.parking"))
                 .build()
-                .apiInfo(metaData());
+                .apiInfo(metaData())
+                .securityContexts(Arrays.asList(getSecurityContext()))
+                .securitySchemes(Arrays.asList(basicAutoScheme()));
+    }
+
+    private SecurityScheme basicAutoScheme(){
+        return new BasicAuth("basicAuth");
+    }
+    private List<SecurityScheme> basicScheme() {
+        List<SecurityScheme> schemeList = new ArrayList<>();
+        schemeList.add(new BasicAuth("basicAuth"));
+        return schemeList;
+    }
+
+    private SecurityReference basicAutoReference() {
+        return new SecurityReference("basicAuth", new AuthorizationScope[0]);
+    }
+
+    private SecurityContext  getSecurityContext() {
+        return SecurityContext.builder().securityReferences(Arrays.asList(basicAutoReference()))
+                .build();
     }
 
     private ApiInfo metaData() {
--- src/test/java/one/digitalinnovation/parking/controller/ParkingControllerTest.java
+++ src/test/java/one/digitalinnovation/parking/controller/ParkingControllerTest.java
@@ -24,6 +24,7 @@ class ParkingControllerTest extends AbstractContainerBase {
     @Test
     void whenFindAllThenCheckResult() {
         RestAssured.given()
+                .header("Autorization: ","")
                 .when()
                 .get("/parking")
                 .then()
@@ -39,6 +40,8 @@ class ParkingControllerTest extends AbstractContainerBase {
         createDTO.setState("SP");
 
         RestAssured.given()
+                .auth()
+                .basic("user","Dio@123456")
                 .when()
                 .contentType(MediaType.APPLICATION_JSON_VALUE)
                 .body(createDTO)
